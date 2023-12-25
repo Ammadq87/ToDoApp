70197283c611658b093c3d11171d1bfb76719c94
Ammad <ammadq87@my.yorku.ca>
2023-12-25 14:36:53 -0500

    Added Fontaweseom library

--- client/src/components/Item.tsx
+++ client/src/components/Item.tsx
@@ -1,9 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 export default function Item() {
     return (
         <>
         
             <div className="Item">
-                
+                <span>hello</span>
+                <FontAwesomeIcon icon={faCoffee}/>
             </div>
 
         </>
